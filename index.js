c22894c509888e0e303e5adde2011c7439ffe7cd
Zhino Salam <zhinosalam188@gmail.com>
2021-05-17 21:31:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
 // Add your code here
-function submitData(name, email) {
-    let formData = {
-        name: name,
-        email: email
-    };
+function fetchBooks() {
 
-    let configObj = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify(formData)
-    };
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+}
 
-    return fetch("http://localhost:3000/users", configObj)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(object) {
-            let h2 = document.createElement('h2');
-            h2.innerHTML = object.id;
-            document.body.appendChild(h2);
-            console.log(object);
-        })
-        .catch(function(error) {
-            let h3 = document.createElement('h3');
-            h3.innerHTML = error.message;
-            document.body.appendChild(h3);
-            console.log(error.message);
-        });
+function renderBooks(books) {
